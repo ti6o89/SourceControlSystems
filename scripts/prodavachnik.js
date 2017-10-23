c68354378e6ffcc6235907e312e31c44a37c7d3f
Stefan Romanov <romanovsc2@gmail.com>
2017-10-23 23:05:56 +0300

    Revert "add branch Notifications"

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,9 +1,4 @@
 function startApp() {
-
-    if (sessionStorage.getItem('authToken') !== null) {
-        let username = sessionStorage.getItem('username');
-        $('#loggedInUser').text("Welcome, " + username + "!");
-    }
     showHideMenuLinks();
     showHomeView();
 
@@ -17,17 +12,6 @@ function startApp() {
     // Bind the form submit buttons
     $("#buttonLoginUser").click(loginUser);
     $("#buttonRegisterUser").click(registerUser);
-
-    // Bind the info / error boxes
-    $("#infoBox, #errorBox").click(function() {
-        $(this).fadeOut();
-    });
-
-    // Attach AJAX "loading" event listener
-    $(document).on({
-        ajaxStart: function() { $("#loadingBox").show() },
-        ajaxStop: function() { $("#loadingBox").hide() }
-    });
 
     const kinveyBaseUrl = "https://mock.api.com/";
     const kinveyAppKey = "kid_rk";
@@ -47,37 +31,13 @@ function startApp() {
             $("#linkRegister").show();
             $("#linkListAds").hide();
             $("#linkLogout").hide();
-            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
             $("#linkListAds").show();
             $("#linkLogout").show();
-            $("#loggedInUser").show();
         }
-    }
-
-    function showInfo(message) {
-        $('#infoBox').text(message);
-        $('#infoBox').show();
-        setTimeout(function() {
-            $('#infoBox').fadeOut();
-        }, 3000);
-    }
-
-    function showError(errorMsg) {
-        $('#errorBox').text("Error: " + errorMsg);
-        $('#errorBox').show();
-    }
-
-    function handleAjaxError(response) {
-        let errorMsg = JSON.stringify(response);
-        if (response.readyState === 0)
-            errorMsg = "Cannot connect due to network error.";
-        if (response.responseJSON && response.responseJSON.description)
-            errorMsg = response.responseJSON.description;
-        showError(errorMsg);
     }
 
     function showHomeView() {
@@ -110,15 +70,13 @@ function startApp() {
             url: kinveyLoginUrl,
             headers: kinveyAuthHeaders,
             data: userData,
-            success: loginSuccess,
-            error: handleAjaxError
+            success: loginSuccess
         });
 
         function loginSuccess(userInfo) {
             saveAuthInSession(userInfo);
             showHideMenuLinks();
             listAdverts();
-            showInfo('Login successful.');
         }
     }
 
@@ -127,9 +85,6 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
-        let username = userInfo.username;
-        sessionStorage.setItem('username', username);
-        $('#loggedInUser').text("Welcome, " + username + "!");
     }
 
     // user/register
@@ -149,8 +104,7 @@ function startApp() {
             url: kinveyRegisterUrl,
             headers: kinveyAuthHeaders,
             data: userData,
-            success: registerSuccess,
-            error: handleAjaxError
+            success: registerSuccess
         });
 
         function registerSuccess(userInfo) {
@@ -158,7 +112,6 @@ function startApp() {
             saveAuthInSession(userInfo);
             showHideMenuLinks();
             listAdverts();
-            showInfo('User registration successful.');
         }
     }
 
@@ -168,7 +121,6 @@ function startApp() {
         $('#loggedInUser').text("");
         showHideMenuLinks();
         showHomeView();
-        showInfo('Logout successful.');
     }
 
     // advertisement/all
@@ -184,12 +136,10 @@ function startApp() {
             method: "GET",
             url: kinveyAdvertsUrl,
             headers: kinveyAuthHeaders,
-            success: loadAdvertsSuccess,
-            error: handleAjaxError
+            success: loadAdvertsSuccess
         });
 
         function loadAdvertsSuccess(adverts) {
-            showInfo('Advertisements loaded.');
             if (adverts.length === 0) {
                 $('#ads').text('No advertisements available.');
             } else {
